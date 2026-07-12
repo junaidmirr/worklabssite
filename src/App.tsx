3954276a9700d0e943ed9714071d5c07896ed2f8
Junaid Mir <89933278+junaidmirr@users.noreply.github.com>
2026-07-12 08:40:53 +0530

    hj

--- src/App.tsx
+++ src/App.tsx
@@ -971,7 +971,7 @@ function Contact() {
                 
                 <div className="mb-6 flex justify-center">
                   <Turnstile 
-                    siteKey="1x00000000000000000000AA" 
+                    siteKey="0x4AAAAAAC94u7BeJiQhw-i8" 
                     onSuccess={(token) => setTurnstileToken(token)} 
                     options={{ theme: dark ? 'dark' : 'light' }}
                   />
